52a1dcdff4f327e94636c4e3cf5086570230d57b
Hallgeirr <frits3012@gmail.com>
2021-02-12 02:04:27 +0200

    OOP task-7 completed

---
+++ 02-OOP/final-keyword.php
@@ -0,0 +1,39 @@
+<?php
+
+class Person
+{
+    const species = 'Homo Sapiens';
+    public $name;
+    public $age;
+    public $occupation;
+
+    function __construct($name, $age, $occupation)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->occupation = $occupation;
+    }
+
+    public function introduce(): string
+    {
+        return "Hello, my name is $this->name";
+    }
+
+    public final function describe_job(): string
+    {
+        return "I am currently working as a(n) $this->occupation";
+    }
+
+    public final static function greet_extraterrestrials($species): string
+    {
+        return "Welcome to Planet Earth $species";
+    }
+}
+
+class ComputerProgrammer extends Person
+{
+    public function introduce(): string
+    {
+        return "Hello, my name is $this->name and I am a $this->occupation";
+    }
+}
