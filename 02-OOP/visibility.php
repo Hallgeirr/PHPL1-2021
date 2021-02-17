5e91a2d109ef29e72e52069b1fca7a13f4fec4f2
Hallgeirr <frits3012@gmail.com>
2021-02-17 15:57:26 +0200

    OOP task-6 fix

--- 02-OOP/visibility.php
+++ 02-OOP/visibility.php
@@ -2,24 +2,29 @@
 
 class Person
 {
-    protected string $name;
-    protected int $age;
-    protected string $occupation;
+    protected $name;
+    protected $age;
+    protected $occupation;
 
     function __construct($name, $age, $occupation)
     {
-        if (!is_string($name)) {
+        $this->name = $this->validData($name, 'name');
+        $this->age = $this->validData($age, 'age');
+        $this->occupation = $this->validData($occupation, 'occupation');
+    }
+
+    public function validData($data, $key)
+    {
+        if ($key == 'name' && !is_string($data)) {
             throw new InvalidArgumentException('Name must be a string!');
         }
-        if (!is_int($age) || $age < 0) {
+        if ($key == 'age' && (!is_int($data) || $data < 0)) {
             throw new InvalidArgumentException('Age must be a non-negative integer!');
         }
-        if (!is_string($occupation)) {
+        if ($key == 'occupation' && !is_string($data)) {
             throw new InvalidArgumentException('Occupation must be a string!');
         }
-        $this->name = $name;
-        $this->age = $age;
-        $this->occupation = $occupation;
+        return $data;
     }
 
     public function get_name(): string
@@ -39,25 +44,19 @@ class Person
 
     public function set_name($name)
     {
-        if (!is_string($name)) {
-            throw new InvalidArgumentException('Name must be a string!');
-        }
+        $this->validData($name, 'name');
         $this->name = $name;
     }
 
     public function set_age($age)
     {
-        if (!is_int($age) || $age < 0) {
-            throw new InvalidArgumentException('Age must be a non-negative integer!');
-        }
+        $this->validData($age, 'age');
         $this->age = $age;
     }
 
     public function set_occupation($occupation)
     {
-        if (!is_string($occupation)) {
-            throw new InvalidArgumentException('Occupation must be a string!');
-        }
+        $this->validData($occupation, 'occupation');
         $this->occupation = $occupation;
     }
 
